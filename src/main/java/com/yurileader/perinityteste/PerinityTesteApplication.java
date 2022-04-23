98e3974f6aae1769359930a21031862e837cc622
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-23 10:21:21 -0300

    Initial commit

---
+++ src/main/java/com/yurileader/perinityteste/PerinityTesteApplication.java
@@ -0,0 +1,13 @@
+package com.yurileader.perinityteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerinityTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerinityTesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurileader/perinityteste/PerinityTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurileader.perinityteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerinityTesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
