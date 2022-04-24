d5bc45e6985bcace9588703d89d45284a26bc8ae
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-24 13:05:48 -0300

    Adicionando Endpoint para atualizar, criar e remover pessoa

--- src/main/java/com/yurileader/perinityteste/controller/PessoaController.java
+++ src/main/java/com/yurileader/perinityteste/controller/PessoaController.java
@@ -1,12 +1,12 @@
 package com.yurileader.perinityteste.controller;
 
 import com.yurileader.perinityteste.dto.PessoaDTO;
+import com.yurileader.perinityteste.dto.PessoaDTOInput;
 import com.yurileader.perinityteste.model.Pessoa;
 import com.yurileader.perinityteste.service.PessoaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequiredArgsConstructor
@@ -19,5 +19,23 @@ public class PessoaController {
     @GetMapping
     public List<PessoaDTO> listarPessoas(){
         return pessoaService.listarTodos();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criarPessoa(@RequestBody PessoaDTOInput pessoaDTO){
+        Pessoa pessoaSalva = pessoaService.criarPessoa(pessoaDTO);
+        return ResponseEntity.ok().body(pessoaSalva);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pessoa> atualizarPessoa(@RequestBody PessoaDTOInput pessoaDTO,@PathVariable Long id){
+        Pessoa pessoaAtualizada = pessoaService.atualizar(pessoaDTO, id);
+        return ResponseEntity.ok().body(pessoaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PessoaDTOInput> removerPessoa(@PathVariable Long id){
+        pessoaService.excluir(id);
+        return ResponseEntity.ok().build();
     }
 }
