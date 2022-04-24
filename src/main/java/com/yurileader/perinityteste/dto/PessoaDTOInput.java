72f07c59cf8a121073319af2a6a9730641d6e08d
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-24 13:06:02 -0300

    Adicionando service para atualizar, criar e remover pessoa

---
+++ src/main/java/com/yurileader/perinityteste/dto/PessoaDTOInput.java
@@ -0,0 +1,15 @@
+package com.yurileader.perinityteste.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PessoaDTOInput {
+
+    private String nome;
+    private String departamento;
+    private Long totalHoras;
+}
--- src/main/java/com/yurileader/perinityteste/service/PessoaService.java
+++ src/main/java/com/yurileader/perinityteste/service/PessoaService.java
@@ -1,9 +1,14 @@
 package com.yurileader.perinityteste.service;
 
 import com.yurileader.perinityteste.dto.PessoaDTO;
+import com.yurileader.perinityteste.dto.PessoaDTOInput;
 import com.yurileader.perinityteste.model.Pessoa;
 import com.yurileader.perinityteste.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +19,38 @@ import java.util.List;
 public class PessoaService {
 
     private final PessoaRepository pessoaRepository;
+    private final ModelMapper modelMapper;
 
     @Transactional(readOnly = true)
     public List<PessoaDTO> listarTodos(){
         return pessoaRepository.listarPessoasHoras();
+    }
+
+    public Pessoa criarPessoa(PessoaDTOInput pessoaDTO) {
+        Pessoa pessoa = toEntity(pessoaDTO);
+        return pessoaRepository.save(pessoa);
+    }
+
+
+    private Pessoa toEntity(PessoaDTOInput dto){
+        return modelMapper.map(dto, Pessoa.class);
+    }
+
+    public Pessoa atualizar(PessoaDTOInput pessoaDTO, Long id) {
+        Pessoa pessoaSalva = buscarPEssoaPorId(id);
+        BeanUtils.copyProperties(pessoaDTO, pessoaSalva);
+        pessoaRepository.save(pessoaSalva);
+        return  pessoaSalva;
+    }
+
+    public Pessoa buscarPEssoaPorId(Long id) {
+        return pessoaRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+    }
+
+    public void excluir(Long id) {
+        Pessoa pessoaSalva = buscarPEssoaPorId(id);
+        if (ObjectUtils.anyNotNull(pessoaSalva) && pessoaSalva.getId() > 0){
+            pessoaRepository.deleteById(id);
+        }
     }
 }
