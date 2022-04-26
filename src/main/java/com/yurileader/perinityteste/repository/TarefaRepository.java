e67f8bc1def9839c01761e103fbef662abbfa8ae
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-25 22:26:14 -0300

    Listando tarefas por id_pessoa

--- src/main/java/com/yurileader/perinityteste/repository/TarefaRepository.java
+++ src/main/java/com/yurileader/perinityteste/repository/TarefaRepository.java
@@ -2,8 +2,15 @@ package com.yurileader.perinityteste.repository;
 
 import com.yurileader.perinityteste.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    @Query("SELECT t FROM Tarefa t WHERE t.pessoaAlocada.id = :id")
+    Optional<List<Tarefa>> listarTarefasPorPessoaId(Long id);
 }
