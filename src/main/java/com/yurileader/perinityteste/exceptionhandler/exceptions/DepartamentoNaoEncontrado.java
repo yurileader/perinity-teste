e1b17e0c679deb8a202e3e82fe5259b5d0d28c87
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-25 22:25:18 -0300

    Adicionando exception de departamento

---
+++ src/main/java/com/yurileader/perinityteste/exceptionhandler/exceptions/DepartamentoNaoEncontrado.java
@@ -0,0 +1,8 @@
+package com.yurileader.perinityteste.exceptionhandler.exceptions;
+
+public class DepartamentoNaoEncontrado extends RuntimeException {
+
+    public DepartamentoNaoEncontrado(String message) {
+        super(message);
+    }
+}
