9bcdfbd9e30225b3b03d30811b1efb13e69e0c54
yuri.oliveira.ext <yurileader@gmail.com>
2022-04-25 21:39:33 -0300

    Adicionando Ajustando DTOS

--- src/main/java/com/yurileader/perinityteste/dto/TarefaDTO.java
+++ src/main/java/com/yurileader/perinityteste/dto/TarefaDTO.java
@@ -1,4 +1,21 @@
 package com.yurileader.perinityteste.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TarefaDTO {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private LocalDate prazo;
+    private DepartamentoDTO departamento;
+    private Long duracao;
+    private PessoaDTO pessoaAlocada;
+    private boolean finalizado;
 }
